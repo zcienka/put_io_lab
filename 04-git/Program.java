fc2b9028170d4b60af19eaf701c5a11037395256
student <p>
2022-10-27 15:29:42 +0200

    zmiana 3

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -5,5 +5,6 @@ class HelloWorld {
         System.out.println("Hello, World!"); 
         System.out.println("Hello, World!"); 
         System.out.println("Hello, World!"); 
+          System.out.println("Hello, World!"); 
     }
 }
