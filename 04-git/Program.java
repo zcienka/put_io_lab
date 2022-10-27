ad8b65c8ed0be0bbc71d1d131444396e09351da3
student <p>
2022-10-27 15:52:50 +0200

    update

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -5,6 +5,6 @@ class HelloWorld {
         System.out.println("Hello, World!"); 
         System.out.println("Hello, World!"); 
         System.out.println("Hello, World!"); 
-          System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!"); System.out.println("Hello, World!"); System.out.println("Hello, World!"); 
     }
 }
